8b95dc370bd8fd643a416eab8e3dc9af98412ca8
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-07-02 14:40:41 -0300

    update main

--- main.c
+++ main.c
@@ -3,34 +3,27 @@
 #include <maze.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
-    if (argc <= 1) {
-        printf("usage: %s <maze.bin>\n", argv[0]);
-        exit(1);
-    }
-    FILE *f = fopen(argv[1], "rb");
+    char input_path[1024];
+
+    scanf("%s%*c", input_path);
+
+    FILE *f = fopen(input_path, "rb");
     if (!f) {
-        printf("error: can not open file '%s'\n", argv[1]);
+        printf("error: can not open file '%s'\n", input_path);
         exit(1);
     }
 
-    int option;
-    printf("1 - DFS\n");
-    printf("2 - BFS\n");
-    printf("3 - A*\n");
-    printf("option: ");
-    scanf("%d", &option);
-
-    char output_path[1024];
-    printf("output path: ");
-    scanf("%s", output_path);
-
-    FILE *output = fopen(output_path, "w");
-    if (!f) {
-        printf("error: can not open file '%s'\n", output_path);
-        exit(1);
-    }
+    // char output_path[1024];
+    // printf("output path: ");
+    // scanf("%s", output_path);
+    // FILE *output = fopen(output_path, "w");
+    // if (!f) {
+    //    printf("error: can not open file '%s'\n", output_path);
+    //    exit(1);
+    //}
 
     Maze  m = maze_make(f);
     Graph g = maze_to_graph(m);
@@ -41,30 +34,33 @@ int main(int argc, char **argv) {
     Point  start;
     Point  end;
 
-    printf("your maze\n\n");
-    maze_show(m);
+    // printf("your maze\n\n");
+    // maze_show(m);
 
-    printf("start: ");
+    // printf("start: ");
     scanf("%d %d", &start.x, &start.y);
-    printf("end: ");
+    // printf("end: ");
     scanf("%d %d", &end.x, &end.y);
 
-    if (option == 1) {
+    char option[20];
+    scanf("%s%*c", option);
+
+    if (!strcmp(option, "DFS")) {
         dfs(g, start, end, path, &expanded, &cost);
-    } else if (option == 2) {
+    } else if (!strcmp(option, "BFS")) {
         bfs(g, start, end, path, &expanded, &cost);
-    } else if (option == 3) {
+    } else if (!strcmp(option, "A*")) {
     } else {
         printf("No algorithm was selected. Leaving.\n");
     }
 
-    maze_save_solution(output, path, cost, expanded);
+    // maze_save_solution(output, path, cost, expanded);
     maze_save_solution(stdout, path, cost, expanded);
 
     graph_destroy(g);
     maze_destroy(m);
     deque_destroy(path);
     fclose(f);
-    fclose(output);
+    // fclose(output);
     return 0;
 }
